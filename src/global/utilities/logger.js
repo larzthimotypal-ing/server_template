b35088421ed6c2e0102c6ab84194fca5acfa2178
LarzPal-ing <148173222+LarzPal-ing@users.noreply.github.com>
2024-09-18 16:48:30 +0800

    regress logging

--- src/global/utilities/logger.js
+++ src/global/utilities/logger.js
@@ -1,32 +1,29 @@
 const pino = require("pino");
 
-const url = require("url");
+// const url = require("url");
 // const __filename = url.fileURLToPath(import.meta.url);
 // const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 // comment pino/file
-const transport = pino.transport({
-  targets: [
-    {
-      target: "pino/file",
-      options: {
-        destination: `${__dirname}../../../app.log`,
-      },
-      level: process.env.FILE_LOG_LEVEL || "info",
-    },
-    {
-      target: "pino-pretty",
-      level: process.env.CONSOLE_LOG_LEVEL || "info",
-    },
-  ],
-});
+// const transport = pino.transport({
+//   targets: [
+//     {
+//       target: "pino/file",
+//       options: {
+//         destination: `${__dirname}../../../app.log`,
+//       },
+//       level: process.env.FILE_LOG_LEVEL || "info",
+//     },
+//     {
+//       target: "pino-pretty",
+//       level: process.env.CONSOLE_LOG_LEVEL || "info",
+//     },
+//   ],
+// });
 
-const logger = pino(
-  {
-    level: process.env.CONSOLE_LOG_LEVEL || "info",
-    timestamp: pino.stdTimeFunctions.isoTime,
-  },
-  transport
-);
+const logger = pino({
+  level: process.env.CONSOLE_LOG_LEVEL || "info",
+  timestamp: pino.stdTimeFunctions.isoTime,
+});
 
 module.exports = logger;
